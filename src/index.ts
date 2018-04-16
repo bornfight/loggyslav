ffb5ab636f32ced035d1e4af090613a4522bfb64
Tomislav Fabeta <tomislav.fabeta@gmail.com>
2018-04-16 10:33:55 +0200

    added export to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from "./core/proxies/ClassLoggerProxy";
 export * from "./core/loggers/SimpleMethodLoggyslav";
 export * from "./core/loggers/ObjectLogger";
 export * from "./core/loggers/PropertyLogger";
+export * from "./loggers/WinstonLoggyslav";
