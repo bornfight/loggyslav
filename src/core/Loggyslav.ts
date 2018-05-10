883a5291cb389279733c732ffbd6a4345f7e2c9c
Tomislav Fabeta <tomislav.fabeta@gmail.com>
2018-05-10 15:42:09 +0200

    renamed Loggyslav interface

--- src/core/Loggyslav.ts
+++ src/core/Loggyslav.ts
@@ -10,8 +10,8 @@ export interface LogClassesInterface {
     properties?: string[];
 }
 
-export interface LogDataConfiguration {
-    classes: LogClassesInterface[];
+export interface TargetsConfiguration {
+    targets: LogClassesInterface[];
 }
 
 export interface LoggerConfiguration {
@@ -20,8 +20,8 @@ export interface LoggerConfiguration {
 }
 
 export class Loggyslav {
-    private logDataConfiguration: LogDataConfiguration = {
-        classes: [],
+    private logDataConfiguration: TargetsConfiguration = {
+        targets: [],
     };
 
     private loggerConfiguration: LoggerConfiguration = {
@@ -30,7 +30,7 @@ export class Loggyslav {
 
     private classLoggers: ClassLoggerProxy[] = [];
 
-    constructor(logDataConfiguration: LogDataConfiguration, loggerConfiguration: LoggerConfiguration) {
+    constructor(logDataConfiguration: TargetsConfiguration, loggerConfiguration: LoggerConfiguration) {
         this.setLogDataConfiguration(logDataConfiguration);
         this.setLoggerConfiguration(loggerConfiguration);
         this.attachLoggers();
@@ -46,7 +46,7 @@ export class Loggyslav {
         this.classLoggers.push(classLogger);
     }
 
-    private setLogDataConfiguration(logDataConfiguration: LogDataConfiguration) {
+    private setLogDataConfiguration(logDataConfiguration: TargetsConfiguration) {
         for (const key in logDataConfiguration) {
             if (logDataConfiguration[key] === undefined) {
                 continue;
@@ -71,7 +71,7 @@ export class Loggyslav {
     }
 
     private attachClassLoggers() {
-        this.logDataConfiguration.classes.forEach((logClass: LogClassesInterface) => {
+        this.logDataConfiguration.targets.forEach((logClass: LogClassesInterface) => {
             const classLoggerProxy = new ClassLoggerProxy(logClass);
             classLoggerProxy.setMethodLogger(this.loggerConfiguration.methodLogger);
 
